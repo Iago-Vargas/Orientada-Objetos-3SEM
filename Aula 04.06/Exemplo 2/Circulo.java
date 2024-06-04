6b1d1ee67363c33bc553117af7aef3df65bb0f0b
Iago-Vargas <166236052+Iago-Vargas@users.noreply.github.com>
2024-06-04 10:34:12 -0300

    Add files via upload

---
+++ Aula 04.06/Exemplo 2/Circulo.java
@@ -0,0 +1,15 @@
+package Exemplo2;
+
+public class Circulo extends Forma{
+	private double raio;
+	
+	public Circulo(double raio) {
+		this.raio = raio;
+	}
+	public double area() {
+		return Math.PI * raio * raio;
+	}
+	public double perimetro() {
+		return 2 * Math.PI *raio;
+	}
+}
---
+++ Aula 04.06/Exemplo 2/Principal.java
@@ -0,0 +1,9 @@
+package Exemplo2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+	}
+
+}
